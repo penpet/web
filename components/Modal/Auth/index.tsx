09eb9712bfd13bffc3c6918230f6d9ae6a15c5f9
kenmueller <kenmueller0@gmail.com>
2021-02-07 13:58:00 -0800

    removed import

--- components/Modal/Auth/index.tsx
+++ components/Modal/Auth/index.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, FormEvent, useState, useCallback } from 'react'
+import { ReactNode, FormEvent, useCallback } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import cx from 'classnames'
