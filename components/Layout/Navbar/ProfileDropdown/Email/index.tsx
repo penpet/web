d996203584b24d0df6fa50576e8b1e10f7178f7a
kenmueller <kenmueller0@gmail.com>
2021-02-20 16:01:56 -0800

    show email

--- components/Layout/Navbar/ProfileDropdown/Email/index.tsx
+++ components/Layout/Navbar/ProfileDropdown/Email/index.tsx
@@ -1,14 +1,25 @@
 import Pal from 'models/Pal'
 
+import styles from './index.module.scss'
+
 export interface ProfileEmailProps {
 	pal: Pal
 }
 
 const ProfileEmail = ({ pal }: ProfileEmailProps) => (
-	<>
-		<label>email</label>
-		<p>{pal.email}</p>
-	</>
+	<div className={styles.root}>
+		<label className={styles.title} htmlFor="profile-dropdown-email-input">
+			email
+		</label>
+		<input
+			id="profile-dropdown-email-input"
+			className={styles.value}
+			readOnly
+			disabled
+			type="email"
+			value={pal.email}
+		/>
+	</div>
 )
 
 export default ProfileEmail
