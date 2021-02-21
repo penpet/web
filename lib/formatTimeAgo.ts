cb743d1a09a16787db00db9d690f4f23a90575d3
kenmueller <kenmueller0@gmail.com>
2021-02-20 19:45:34 -0800

    removed future dates

--- lib/formatTimeAgo.ts
+++ lib/formatTimeAgo.ts
@@ -4,6 +4,7 @@ import locale from 'javascript-time-ago/locale/en'
 TimeAgo.addLocale(locale)
 const timeAgo = new TimeAgo('en-US')
 
-const formatTimeAgo = (date: Date) => timeAgo.format(date)
+const formatTimeAgo = (date: Date) =>
+	timeAgo.format(Math.min(Date.now(), date.getTime()))
 
 export default formatTimeAgo
