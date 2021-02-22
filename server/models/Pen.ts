9985b6e53ac0b32564d916fbbaba2a6dcbef158c
kenmueller <kenmueller0@gmail.com>
2021-02-21 18:40:29 -0800

    cascade pen deletes instead of manual delete

--- server/models/Pen.ts
+++ server/models/Pen.ts
@@ -186,50 +186,11 @@ export const deletePen = async (
 	if (role !== Role.Owner)
 		throw new HttpError(403, 'You must be the owner of this pen to delete it')
 
-	try {
-		await client.query('BEGIN')
-
-		await Promise.all([
-			client.query<Record<string, never>, [string]>(
-				`
-				DELETE FROM invites
-				WHERE pen_id = $1
-				`,
-				[penId]
-			),
-			client.query<Record<string, never>, [string]>(
-				`
-				DELETE FROM roles
-				WHERE pen_id = $1
-				`,
-				[penId]
-			),
-			client.query<Record<string, never>, [string]>(
-				`
-				DELETE FROM operations
-				WHERE id = $1
-				`,
-				[penId]
-			),
-			client.query<Record<string, never>, [string]>(
-				`
-				DELETE FROM snapshots
-				WHERE id = $1
-				`,
-				[penId]
-			),
-			client.query<Record<string, never>, [string]>(
-				`
-				DELETE FROM pens
-				WHERE id = $1
-				`,
-				[penId]
-			)
-		])
-
-		await client.query('COMMIT')
-	} catch (error) {
-		await client.query('ROLLBACK')
-		throw error
-	}
+	await client.query<Record<string, never>, [string]>(
+		`
+		DELETE FROM pens
+		WHERE id = $1
+		`,
+		[penId]
+	)
 }
